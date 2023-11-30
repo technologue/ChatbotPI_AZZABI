e4c71c08040798636037e006e9d72a27bd6a0022
Mohamed Seifeddine Azzabi <mohamedseiffedine@yahoo.fr>
2023-11-30 20:20:33 +0100

    V1.301123

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -2,9 +2,8 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import nltk
 app = FastAPI()
-
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,6 +12,19 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+def analyze_texte(texte :str):
+    mot_cle=nltk.word_tokenize(texte)
+    return {"sujet":"vide","sentiments":[],"mot_cles":mot_cle}
+
+def generer_reponse(texte: str):
+    return {"reponse":"reponse vide"}
+
+def formater_reponse(texte: str):
+    return {"reponse_formater":"reponse vide formater"}
+
+
 
 class AnalyseTexteInput(BaseModel):
     texte: str
